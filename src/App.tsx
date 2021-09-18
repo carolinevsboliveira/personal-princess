87e70e2238c21f7035212c837bccd4b6025c808c
carolinevsboliveira <carolinevsboliveira@gmail.com>
2021-09-18 01:07:05 -0400

    Side to side cards applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Cards from './components/Cards';
+import CardGrid from './components/CardsGrid';
 function App() {
+  const docs = [1 ,2, 4, 4, 5, 6,6,7,7,7]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardGrid />
     </div>
   );
 }
