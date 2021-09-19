19e3e485d9e04b42956a1182202ef8948dab75c3
carolinevsboliveira <carolinevsboliveira@gmail.com>
2021-09-18 21:23:10 -0400

    Fixed align bugs

--- src/components/CardsGrid.tsx
+++ src/components/CardsGrid.tsx
@@ -1,26 +1,29 @@
 import { Grid } from "semantic-ui-react";
 import useFirestore from "../hooks/useFirestore";
 import Cards from "./Cards";
+import { GridBox } from "./Styles/MainStyle";
 const CardGrid = () => {
   const { documents } = useFirestore("images");
   return (
-    <Grid>
-      {documents.map((doc: any) => (
-        <div key={doc != null ? doc.key : 0}>
-        {doc != null &&
-          <Grid.Row>
-            <Grid.Column>
-              <Cards
-                description={doc.description}
-                path={doc.url_squared}
-                title={doc.title}
-              />
-            </Grid.Column>
-          </Grid.Row>
-        }
-        </div>
-      ))}
-    </Grid>
+    <GridBox>
+      <Grid centered>
+        {documents.map((doc: any) => (
+          <div key={doc != null ? doc.key : 0}>
+            {doc != null && (
+              <Grid.Row>
+                <Grid.Column>
+                  <Cards
+                    description={doc.description}
+                    path={doc.url_squared}
+                    title={doc.title}
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            )}
+          </div>
+        ))}
+      </Grid>
+    </GridBox>
   );
 };
 
