bcb963b02da6152098b32f9a59534a58e27df1bb
carolinevsboliveira <carolinevsboliveira@gmail.com>
2021-09-18 23:44:33 -0400

    Fixed cards padding

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,19 +1,20 @@
-import { Card, Image } from 'semantic-ui-react'
-interface iProps{
-    path: string,
-    description: string, 
-    title: string
+import { Card, Image } from "semantic-ui-react";
+import { CardBox } from "./Styles/MainStyle";
+interface iProps {
+  path: string;
+  description: string;
+  title: string;
 }
 const Cards = (props: iProps) => (
-  <Card>
-    <Image src={props.path} wrapped ui={false}/>
-    <Card.Content>
-      <Card.Header>{props.title}</Card.Header>
-      <Card.Description>
-        {props.description}
-      </Card.Description>
-    </Card.Content>
-  </Card>
-)
+  <CardBox>
+    <Card>
+      <Image src={props.path} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{props.title}</Card.Header>
+        <Card.Description>{props.description}</Card.Description>
+      </Card.Content>
+    </Card>
+  </CardBox>
+);
 
-export default Cards
+export default Cards;
